a9190f3ce8d573d9a2eed6bcdb23eafbcf0ed57d
Kazuma <aoki.kazuma.s1@elms.hokudai.ac.jp>
2023-02-22 13:14:27 +0900

    2問目作成

--- src/views/QuizApp.vue
+++ src/views/QuizApp.vue
@@ -1,24 +1,18 @@
 <template>
   <h1>Vue クイズ</h1>
   <div class="app">
-    <h2>Q. {{ quiz.text }}</h2>
-    <img
-      class="quiz-image"
-      src="https://via.placeholder.com/300x300"
-      alt="クイズタイトル"
-    />
+    <h2>Q. {{ quizs[quiz_num].text }}</h2>
+    <img class="quiz-image" v-bind:src="quizImagePath" alt="クイズタイトル" />
     <div class="container">
-      <button>
-        {{ quiz.choices[0].text }}
-      </button>
-      <button>
-        {{ quiz.choices[1].text }}
-      </button>
-      <button>
-        {{ quiz.choices[2].text }}
+      <button
+        v-for="(choice, i) in quizs[quiz_num].choices"
+        v-bind:key="i"
+        v-on:click="choiced(choice)"
+      >
+        {{ choice.text }}
       </button>
     </div>
-    <div>{{ "答え" }}</div>
+    <div>{{ feedback }}</div>
   </div>
 </template>
 
@@ -26,22 +20,66 @@
 export default {
   data() {
     return {
-      quiz: {
-        text: "このキャラクターは誰でしょう",
-        image: "",
-        choices: [
-          {
-            text: "リツコ",
-          },
-          {
-            text: "綾波レイ",
-          },
-          {
-            text: "アスカ",
-          },
-        ],
-      },
+      feedback: "",
+      quiz_num: 0,
+      quizs: [
+        {
+          text: "このキャラクターは誰でしょう",
+          image: "rei.jpg",
+          choices: [
+            {
+              text: "リツコ",
+              isCorrect: false,
+              feedback: "残念。リツコはもっと年をっています。",
+            },
+            {
+              text: "綾波レイ",
+              isCorrect: true,
+              feedback: "正解。この写真は綾波レイです。",
+            },
+            {
+              text: "アスカ",
+              isCorrect: false,
+              feedback: "残念。アスカは",
+            },
+          ],
+        },
+        {
+          text: "1枚目の写真の信頼度は何%でしょうか",
+          image: "rei.jpg",
+          choices: [
+            {
+              text: "15%",
+              isCorrect: false,
+              feedback: "残念。もっとアツいです。",
+            },
+            {
+              text: "67%",
+              isCorrect: false,
+              feedback: "残念。もう少しアツいです。",
+            },
+            {
+              text: "83%",
+              isCorrect: true,
+              feedback: "正解。こんなに信頼度高いけど、意外と外れます",
+            },
+          ],
+        },
+      ],
     }
+  },
+  methods: {
+    choiced(choice) {
+      this.feedback = choice.feedback
+      if (choice.isCorrect && this.quiz_num != length(this.quizs) - 1) {
+        this.quiz_num += 1
+      }
+    },
+  },
+  computed: {
+    quizImagePath() {
+      return "./assets/" + this.quiz.image
+    },
   },
 }
 </script>
